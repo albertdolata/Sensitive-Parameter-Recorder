35d7bc9d9064f9110b6e622ace1b851e94dd6441
Albert Dolata <albdolcode@gmail.com>
2026-04-02 13:30:29 +0200

    Refactor: add macro for number of sensors, add starting logs, fix delay logic

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,29 @@
 #include "AccelSensor.h"
 #include "TempHumSensor.h"
 
-Sensor* sensors[3];
+#define NUM_SENSORS 4
+
+Sensor* sensors[NUM_SENSORS];
 
 void setup() {
     Serial.begin(115200);
+    delay(1000);
+    Serial.println("\n ----------- Rejestrator uruchomiony -----------");
 
     sensors[0] = new TempHumSensor("bme280 Centrala Główna", 0x76);
     sensors[1] = new AccelSensor("Paleta szkło", 1);
     sensors[2] = new AccelSensor("Paleta telewizory", 2);
     sensors[3] = new AccelSensor("Paleta piwo", 3);
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < NUM_SENSORS; i++) {
         sensors[i]->initialize();
     }
 }
 
 void loop() {
-    for (int i = 0; i < 3; i++) {
+    Serial.println("\n ------- Odczyt z czujników -------");
+    for (int i = 0; i < NUM_SENSORS; i++) {
         sensors[i]->readData();
-        delay(3000);
     }
+    delay(3000);
 }
