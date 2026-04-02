f9c7353d5894c042bb2d34f656a19b3f13079c33
Albert Dolata <albdolcode@gmail.com>
2026-04-02 14:27:55 +0200

    Change logic of function readData in ContactSensor

---
+++ include/ContactSensor.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Sensor.h"
+
+class ContactSensor : public Sensor {
+   private:
+    bool isDoorOpen;
+
+   public:
+    ContactSensor(String name) : Sensor(name) {}
+
+    bool initialize() override {
+        return true;
+    }
+
+    void readData() override {
+        isDoorOpen = random(0, 2);
+        if (isDoorOpen)
+            Serial.printf("UWAGA! %s wykrył, że drzwi są otwarte \n",
+                          sensorName.c_str());
+    }
+};
