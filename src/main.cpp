d30fbde266855668e5ce17a67d5abc659f40a393
Albert Dolata <albdolcode@gmail.com>
2026-04-13 09:28:35 +0200

    Refactor: Update code to suppport new BLESensors

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include <Arduino.h>
 
 #include "AccelSensor.h"
-#include "TempHumSensor.h"
-#include "ContactSensor.h"
-#include "PresenceSensor.h"
-#include "CloudManager.h"
-#include "secrets.h"
 #include "BLESensor.h"
 #include "BLESensorManager.h"
+#include "CloudManager.h"
+#include "ContactSensor.h"
+#include "PresenceSensor.h"
+#include "TempHumSensor.h"
+#include "secrets.h"
 
-#define NUM_SENSORS 7
+#define NUM_SENSORS 6
 
 Sensor* sensors[NUM_SENSORS];
 CloudManager cloud;
@@ -27,12 +27,11 @@ void setup() {
     cloud.initCloud();
 
     sensors[0] = new TempHumSensor("bme280 Centrala Główna", 0x76);
-    sensors[1] = new AccelSensor("Paleta szkło", 1);
-    sensors[2] = new AccelSensor("Paleta telewizory", 2);
-    sensors[3] = new AccelSensor("Paleta piwo", 3);
+    sensors[1] = new BLESensor("Paleta szkło");
+    sensors[2] = new BLESensor("Paleta telewizory");
+    sensors[3] = new BLESensor("Paleta piwo");
     sensors[4] = new ContactSensor("Kontaktron");
     sensors[5] = new PresenceSensor("Czujnik obecności człowieka");
-    sensors[6] = new BLESensor("NRF1");
 
     for (int i = 0; i < NUM_SENSORS; i++) {
         sensors[i]->initialize();
@@ -40,7 +39,7 @@ void setup() {
 
     BLEDevice::init("");
     pBLEScan = BLEDevice::getScan();
-    radarBLE = new BLESensorManager(sensors[6]);
+    radarBLE = new BLESensorManager(sensors[1], sensors[2], sensors[3]);
 
     pBLEScan->setAdvertisedDeviceCallbacks(radarBLE);
     pBLEScan->setActiveScan(true);
@@ -53,28 +52,27 @@ void loop() {
     pBLEScan->start(6, false);
     pBLEScan->clearResults();
 
-
-
     Serial.println("\n ------- Odczyt z czujników -------");
     for (int i = 0; i < NUM_SENSORS; i++) {
         sensors[i]->readData();
     }
 
-    ThingSpeak.setField(1, sensors[0]->value1); //temperatura z BME
-    ThingSpeak.setField(2, sensors[0]->value3); //wilgotność z BME
-    ThingSpeak.setField(3, sensors[0]->value2); //ciśnienie z BME
-    ThingSpeak.setField(4, sensors[1]->value1); //wstrzas z paleta szkło
-    ThingSpeak.setField(5, sensors[2]->value1); //wstrzas z paleta telewizroy
-    ThingSpeak.setField(6, sensors[3]->value1); //wstrzas z paleta piwo
-    ThingSpeak.setField(7, sensors[4]->value1); //kontaktron
-    ThingSpeak.setField(8, sensors[5]->value1); //obecność człowieka
+    ThingSpeak.setField(1, sensors[0]->value1);  // temperatura z BME
+    ThingSpeak.setField(2, sensors[0]->value2);  // wilgotność z BME
+    ThingSpeak.setField(3, sensors[0]->value3);  // ciśnienie z BME
+    ThingSpeak.setField(4, sensors[1]->value1);  // Temperatura z paleta szkło BLESensor
+    ThingSpeak.setField(5, sensors[2]->value1);  // Temperatura z paleta telewizroy BLESensor
+    ThingSpeak.setField(6, sensors[3]->value1);  // Temperatura z paleta piwo BLESensor
+    ThingSpeak.setField(7, sensors[4]->value1);  // kontaktron
+    ThingSpeak.setField(8, sensors[5]->value1);  // obecność człowieka
 
     Serial.println("Wysyłanie danych do ThingSpeak");
     serverRespone = ThingSpeak.writeFields(channelNumber, writeAPIKey);
-    if( serverRespone == 200)
-    Serial.println("Dane zostały wysłane");
+    if (serverRespone == 200)
+        Serial.println("Dane zostały wysłane");
     else
-    Serial.printf("Coś poszło nie tak, kod błędu: %d", serverRespone);
+        Serial.printf("Coś poszło nie tak, kod błędu: %d", serverRespone);
 
-    delay(20000); // tak dużo bo ThingSpeak na darmowym konicie wysyła co 15 sek dane
+    delay(20000);  // tak dużo bo ThingSpeak na darmowym konicie wysyła co 15
+                   // sek dane
 }
