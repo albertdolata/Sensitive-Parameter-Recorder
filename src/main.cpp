09f0980b5c63f22aaf54058b1bac1ca63892daa7
Albert Dolata <albdolcode@gmail.com>
2026-04-14 19:52:57 +0200

    Update: the data that is set in the ThingSpeak fields has been changed

--- src/main.cpp
+++ src/main.cpp
@@ -81,12 +81,12 @@ void loop() {
 
     ThingSpeak.setField(1, sensors[0]->value1);  // temperatura z BME
     ThingSpeak.setField(2, sensors[0]->value2);  // wilgotność z BME
-    ThingSpeak.setField(3, sensors[0]->value3);  // ciśnienie z BME
-    ThingSpeak.setField(4, sensors[1]->value1);  // Temperatura z paleta szkło BLESensor
-    ThingSpeak.setField(5, sensors[2]->value1);  // Temperatura z paleta telewizroy BLESensor
+    ThingSpeak.setField(3, sensors[1]->value1);  // Temperatura z paleta szkło BLESensor
+    ThingSpeak.setField(4, sensors[1]->value2);  // Wilgotność z paleta szkło BLESensor
+    ThingSpeak.setField(5, sensors[1]->value3);  // Wychylenie z paleta telewizroy BLESensor
     ThingSpeak.setField(6, sensors[3]->value1);  // Temperatura z paleta piwo BLESensor
-    ThingSpeak.setField(7, sensors[4]->value1);  // kontaktron
-    ThingSpeak.setField(8, sensors[5]->value1);  // obecność człowieka
+    ThingSpeak.setField(7, sensors[4]->value1);  // kontaktron (mocked)
+    ThingSpeak.setField(8, sensors[5]->value1);  // obecność człowieka (mocked)
 
     Serial.println("Wysyłanie danych do ThingSpeak");
     serverRespone = ThingSpeak.writeFields(channelNumber, writeAPIKey);
