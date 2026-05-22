edc702bb10060ec85833466e48ad74311d382fbb
Albert Dolata <albdolcode@gmail.com>
2026-05-22 10:59:12 +0200

    Expand json format in data_send_service

--- src/data_send_service.cpp
+++ src/data_send_service.cpp
@@ -1,52 +1,61 @@
 #include "data_send_service.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/queue.h"
-#include "esp_log.h"
+
 #include <string.h>
 
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
 #include "sim7000_core.h"
 
-static const char *TAG = "DATA_SERVICE";
+static const char* TAG = "DATA_SERVICE";
 static QueueHandle_t data_queue = NULL;
 
-
-static void data_sender_task(void *pvParameters) {
+static void data_sender_task(void* pvParameters) {
     sensor_data_t incoming_data;
-    char json_buffer[256];
+    char json_buffer[512];
     bool mqtt_connected = false;
 
     while (1) {
         if (xQueuePeek(data_queue, &incoming_data, portMAX_DELAY)) {
-            
-            if(!sim7000_wait_for_network()){
+            if (!sim7000_wait_for_network()) {
                 vTaskDelay(pdMS_TO_TICKS(5000));
                 continue;
             }
 
-            if(!mqtt_connected){
-                if(sim7000_mqtt_connect()){
+            if (!mqtt_connected) {
+                if (sim7000_mqtt_connect()) {
                     mqtt_connected = true;
-                }else{
-                    ESP_LOGW(TAG,"Błąd negocjacji MQTT. Ponowienie za 5s.");
+                } else {
+                    ESP_LOGW(TAG, "Błąd negocjacji MQTT. Ponowienie za 5s.");
                     vTaskDelay(pdMS_TO_TICKS(5000));
                     continue;
-                }    
+                }
             }
 
             snprintf(json_buffer, sizeof(json_buffer),
-            "{\"temp\":%.2f,\"hum\":%.2f,\"mcc\":%u,\"mnc\":%u,\"tac\":%lu,\"cid\":%lu}",
-            incoming_data.temperature, incoming_data.humidity,
-            incoming_data.cell_info.mcc, incoming_data.cell_info.mnc,
-            incoming_data.cell_info.tac, incoming_data.cell_info.cid);
+                     "{\"temp_m\":%.2f,\"hum_m\":%.2f,\"shock_m\":%.2f,\"temp_"
+                     "s\":%.2f,\"hum_s\":%.2f,\"shock_s\":%.2f,\"lat\":%.6f,"
+                     "\"lon\":%.6f,\"shock_p1\":%.2f,"
+                     "\"mcc\":%u,\"mnc\":%u,\"tac\":%lu,\"cid\":%lu}",
+                     incoming_data.temperature_main_central,
+                     incoming_data.humidity_main_central,
+                     incoming_data.shock_level_main_central,
+                     incoming_data.temperature_secondary_central,
+                     incoming_data.humidity_secondary_central,
+                     incoming_data.shock_level_secondary_central,
+                     incoming_data.latitude, incoming_data.longitude,
+                     incoming_data.shock_level_palette1,
+                     incoming_data.cell_info.mcc, incoming_data.cell_info.mnc,
+                     incoming_data.cell_info.tac, incoming_data.cell_info.cid);
 
             ESP_LOGI(TAG, "Próba wysyłki: %s", json_buffer);
 
-            if(sim7000_mqtt_send("dom/czujnik1",json_buffer)){
-                ESP_LOGI(TAG,"Pakiet wysłany pomyślnie.");
+            if (sim7000_mqtt_send("dom/czujnik1", json_buffer)) {
+                ESP_LOGI(TAG, "Pakiet wysłany pomyślnie.");
                 xQueueReceive(data_queue, &incoming_data, 0);
-            }else{
-                ESP_LOGE(TAG,"Błąd wysyłania MQTT. Retransmisja");
+            } else {
+                ESP_LOGE(TAG, "Błąd wysyłania MQTT. Retransmisja");
                 mqtt_connected = false;
                 vTaskDelay(pdMS_TO_TICKS(5000));
             }
@@ -58,7 +67,7 @@ void data_service_init(void) {
     xTaskCreate(data_sender_task, "data_sender_task", 4096, NULL, 5, NULL);
 }
 
-bool data_service_push(sensor_data_t *data) {
+bool data_service_push(sensor_data_t* data) {
     if (data_queue == NULL) return false;
     return xQueueSend(data_queue, data, 0) == pdPASS;
 }
