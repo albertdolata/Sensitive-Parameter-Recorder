b94b1bf3bbd597e8e2b51f4e8d8fd4028f844ad0
Albert Dolata <albdolcode@gmail.com>
2026-05-22 10:13:37 +0200

    Refactor of class GPSManager - class now is using sent_at_cmd function

--- include/GPSManager.h
+++ include/GPSManager.h
@@ -1,22 +1,20 @@
 #pragma once
 #include <Arduino.h>
 
+#include "sim7000_core.h"
+
 class GPSManager {
    private:
-    HardwareSerial& modemSerial;
-    int rxPin;
-    int txPin;
-    uint32_t baudRate;
-    float latitude;
-    float longtitude;
+    double latitude;
+    double longtitude;
     bool isON;
-    String rxBuffer;
+
     void parseResponse(String response) {
         int commaCounter;
         String rawTime;
         String rawLatitdue;
         String rawLongtitude;
-        if (response.startsWith("+CGNSINF:")) {
+        if (response.indexOf("+CGNSINF:") != -1) {
             commaCounter = response.indexOf(',');
             int nextComma = response.indexOf(',', commaCounter + 1);
             if (response.substring(commaCounter + 1, nextComma) == "1") {
@@ -31,8 +29,8 @@ class GPSManager {
                 nextComma = response.indexOf(',', commaCounter + 1);
                 rawLongtitude = response.substring(commaCounter + 1, nextComma);
 
-                latitude = rawLatitdue.toFloat();
-                longtitude = rawLongtitude.toFloat();
+                latitude = rawLatitdue.toDouble();
+                longtitude = rawLongtitude.toDouble();
             } else {
                 isON = false;
             }
@@ -40,71 +38,30 @@ class GPSManager {
     }
 
    public:
-    GPSManager(HardwareSerial& serial, int rxPin, int txPin, uint32_t baud)
-        : modemSerial(serial),
-          rxPin(rxPin),
-          txPin(txPin),
-          baudRate(baud),
-          latitude(0.0),
-          longtitude(0.0),
-          isON(false) {}
+    GPSManager() : latitude(0.0), longtitude(0.0), isON(false) {}
 
     void begin() {
-        pinMode(27, OUTPUT);
-        digitalWrite(27, HIGH);
-        delay(2000);
-        digitalWrite(27, LOW);
-        delay(3000);
+        char rx_buff[128];
 
-        modemSerial.begin(baudRate);
-        Serial.println("\n[DEBUG] Czekam na start modemu");
+        send_at_cmd("AT+CGNSPWR=1\r\n", rx_buff, sizeof(rx_buff), 2000);
 
-        bool isResponding = false;
-        while (!isResponding) {
-            modemSerial.println("AT");
-            delay(500);
-
-            while (modemSerial.available()) {
-                String odpowiedz = modemSerial.readString();
-                if (odpowiedz.indexOf("OK") != -1) {
-                    isResponding = true;
-                    Serial.println(
-                        "[DEBUG] Modem sie włączył");
-                }
-            }
-        }
-
-        modemSerial.println("AT+CGNSPWR=1");
-        delay(1000);
-
-        while (modemSerial.available()) modemSerial.read();
         Serial.println("[DEBUG] GPS pomyslnie zainicjowany.");
     }
 
     void update() {
-        while (modemSerial.available()) {
-            char c = modemSerial.read();
-            Serial.print(c);
-            rxBuffer += c;
-            if (c == '\n') {
-                rxBuffer.trim();
-                if (rxBuffer.length() > 0) {
-                    parseResponse(rxBuffer);
-                }
-                rxBuffer = "";
-            }
+        char rx_buff[256];
+
+        if(send_at_cmd("AT+CGNSINF\r\n", rx_buff, sizeof(rx_buff), 2000) > 0) {
+            String response(rx_buff);
+            parseResponse(response);
         }
     }
 
-    void requestPosition() {
-        modemSerial.println("AT+CGNSINF");
-    }
-
-    float getLatitude() {
+    double getLatitude() {
         return latitude;
     }
 
-    float getLongtitude() {
+    double getLongtitude() {
         return longtitude;
     }
 
