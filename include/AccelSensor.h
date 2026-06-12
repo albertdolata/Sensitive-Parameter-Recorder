5f3000586080fef2413d281ded4ec4f5c39d7736
Albert Dolata <albdolcode@gmail.com>
2026-06-13 00:51:04 +0200

    Refacotr: update class for LIS3DH accelerometer

--- include/AccelSensor.h
+++ include/AccelSensor.h
@@ -1,28 +1,42 @@
 #pragma once
 
+#include <Adafruit_LIS3DH.h>
+
 #include "Sensor.h"
 
 class AccelSensor : public Sensor {
    private:
-    int idPalete;
+    Adafruit_LIS3DH lis3dh;
+    uint8_t i2c_address;
+    float axisX;
+    float axisY;
+    float axisZ;
 
    public:
-    AccelSensor(String name, int id) : Sensor(name) {
-        idPalete = id;
-    }
+    AccelSensor(uint8_t addr)
+        : i2c_address(addr), axisX(0.0), axisY(0.0), axisZ(0.0) {}
 
     bool initialize() override {
-        return true;
+        return lis3dh.begin(i2c_address);
+        //lis3dh.setRange(LIS3DH_RANGE_2_G); //do ustalenia zakres pomiarowy
     }
 
     void readData() override {
-        int shocklvl = random(0, 100);
-        value1 = shocklvl;
-        if (shocklvl > 50) {
-            Serial.printf("Paleta nr %d %s -> Zarejestrowano wstrząs o sile: %.2d \n",
-                          idPalete, sensorName.c_str(), shocklvl);
-        } else {
-            Serial.println("nie zarejestrowano gwałtownego wstrząsu");
+        if (lis3dh.haveNewData()) {
+            lis3dh.read();
+            axisX = lis3dh.x_g;
+            axisY = lis3dh.y_g;
+            axisZ = lis3dh.z_g;
         }
+    }
+
+    float getAxisX() const {
+        return axisX;
+    }
+    float getAxisY() const {
+        return axisY;
+    }
+    float getAxisZ() const {
+        return axisZ;
     }
 };
