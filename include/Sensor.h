db53ff6a4f2350baba35d149ba56c7c87700c8d1
Albert Dolata <albdolcode@gmail.com>
2026-06-13 00:17:13 +0200

    Delete fields from abstarct class Sensor

--- include/Sensor.h
+++ include/Sensor.h
@@ -1,19 +1,9 @@
 #pragma once
-#include <Arduino.h>
 
 class Sensor {
-protected:
-String sensorName;
-
 public:
-//tymczasowe pola dla wartości z czujników - w przyszłości do zmiany może na jakąś tablice do przemyślenia
-float value1 = 0;
-float value2 = 0;
-float value3 = 0;
-float value4 = 0;
-Sensor(String name){
-    sensorName = name;
-}
+
+Sensor(){}
 
 virtual ~Sensor() {}
 
