46bde00caff7becf14fcb2963496c0330a732edb
Albert Dolata <albdolcode@gmail.com>
2026-06-16 02:21:07 +0200

    Refactor: Integrate MainCentralSensorManager and clean up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,22 @@
 #include <Arduino.h>
 
-#include "AccelSensor.h"
-#include "BLESensor.h"
-#include "BLESensorManager.h"
-#include "ContactSensor.h"
 #include "GPSManager.h"
-#include "PresenceSensor.h"
-#include "TempHumSensor.h"
 #include "data_send_service.h"
-#include "secrets.h"
 #include "sys/time.h"
 #include "SPIFFS.h"
+#include "MainCentralSensorManager.h"
 
-#define NUM_SENSORS 6
 #define SIM_RX_PIN GPIO_NUM_16
 #define SIM_TX_PIN GPIO_NUM_17
 #define SIM_PWR_PIN GPIO_NUM_27
 
-Sensor* sensors[NUM_SENSORS];
-BLESensorManager* radarBLE;
+#define PRESENCE_SENSOR_PIN 14
+#define ACCEL_SENSOR_I2C_ADDR 0x18
+#define PALLET_NODE_ID_1 0x01
+#define PALLET_NODE_ID_2 0x02
+
+MainCentralSensorManager centralSensorManager(PRESENCE_SENSOR_PIN, ACCEL_SENSOR_I2C_ADDR, PALLET_NODE_ID_1, PALLET_NODE_ID_2);
+
 BLEScan* pBLEScan;
 GPSManager GPS;
 
@@ -103,21 +101,14 @@ void sendBackupData() {
     }
 }
 
-void assignDataToStruct(sensor_data_t* data, GPSManager* gps, Sensor* sensors[]) {
+void assignSimComDataToStruct(sensor_data_t* data, GPSManager* gps) {
     data->cell_info = sim7000_get_network_params();
     data->latitude = gps->getLatitude();
     data->longitude = gps->getLongitude();
-    data->temperature_main_central = sensors[0]->value1;
-    data->humidity_main_central = sensors[0]->value2;
-    data->shock_level_main_central = sensors[1]->value1;
-    data->shock_level_palette1 = sensors[1]->value1;
     // narazie nie ma tych czujników, ale zostawiam miejsce w strukturze i
     // kodzie, żeby łatwo było dodać w przyszłości
     // data->shock_level_palette2 = sensors[2]->value1;
     // data->shock_level_palette3 = sensors[3]->value1;
-    data->temperature_secondary_central = 21.37;
-    data->humidity_secondary_central = 67.69;
-    data->shock_level_secondary_central = 0.66;
     data->timestamp = time(NULL);
 }
 
@@ -136,31 +127,11 @@ void SIMComInit() {
 void BLEInit() {
     BLEDevice::init("");
     pBLEScan = BLEDevice::getScan();
-    radarBLE = new BLESensorManager(sensors[1], sensors[2], sensors[3]);
 
-    pBLEScan->setAdvertisedDeviceCallbacks(radarBLE);
+    pBLEScan->setAdvertisedDeviceCallbacks(centralSensorManager.getBLESensorManager());
     pBLEScan->setActiveScan(true);
     pBLEScan->setInterval(100);
     pBLEScan->setWindow(99);
-}
-
-void SensorTableInit(Sensor* sensor[]) {
-    sensor[0] = new TempHumSensor("bme280 Centrala Główna", 0x76);
-    sensor[1] = new BLESensor("Paleta szkło");
-    sensor[2] = new BLESensor("Paleta telewizory");
-    sensor[3] = new BLESensor("Paleta piwo");
-    sensor[4] = new ContactSensor("Kontaktron");
-    sensor[5] = new PresenceSensor("Czujnik obecności człowieka");
-
-    for (int i = 0; i < NUM_SENSORS; i++) {
-        sensor[i]->initialize();
-    }
-}
-
-void readDataFromSensor(Sensor* sensor[]) {
-    for (int i = 0; i < NUM_SENSORS; i++) {
-        sensor[i]->readData();
-    }
 }
 
 void sendDataToServer(sensor_data_t* current_data) {
@@ -168,6 +139,14 @@ void sendDataToServer(sensor_data_t* current_data) {
         saveDataOffline(current_data);
     } else {
         sendBackupData();
+    }
+}
+
+void checkAndUpdateTime(GPSManager* gps, uint32_t* last_gps_time) {
+    if (gps->hasFix() && gps->getTimestamp() != *last_gps_time) {
+        setESP32Time(gps->getTimestamp());
+        *last_gps_time = gps->getTimestamp();
+        Serial.println("Czas ESP32 zaktualizowany na podstawie GPS.");
     }
 }
 
@@ -184,7 +163,7 @@ void setup() {
 
     data_service_init();
 
-    SensorTableInit(sensors);
+    centralSensorManager.initializeAllSensors();
 
     BLEInit();
 
@@ -195,22 +174,18 @@ void loop() {
     static uint32_t last_gps_time = 0;
 
     GPS.update();
-    if(GPS.hasFix()) {
-        uint32_t current_gps_time = GPS.getTimestamp();
-        if(current_gps_time != last_gps_time) {
-            setESP32Time(current_gps_time);
-            last_gps_time = current_gps_time;
-        }
-    }
+    
+    checkAndUpdateTime(&GPS, &last_gps_time);
 
     Serial.println("\n ------- Nasłuch BLE -------");
     BLEScanResults foundDevices = pBLEScan->start(6, false);
     pBLEScan->clearResults();
 
     Serial.println("\n ------- Odczyt z czujników -------");
-    readDataFromSensor(sensors);
-
-    assignDataToStruct(&current_data, &GPS, sensors);
+    centralSensorManager.readAllSensorsData();
+    centralSensorManager.fillSensorData(&current_data);
+    
+    assignSimComDataToStruct(&current_data, &GPS);
 
     Serial.println("\n ------- Wysyłka danych -------");
     sendDataToServer(&current_data);
